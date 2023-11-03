a7c47cc2b03343fab47a88aaed7f3ee37953d945
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-11-03 20:45:32 +0100

    add create user function and connect to firestore

--- src/screens/UserForm/UserForm.tsx
+++ src/screens/UserForm/UserForm.tsx
@@ -1,10 +1,41 @@
+import React, { useState } from "react";
 import { View, Text } from "react-native";
 import { Card, Input } from "@rneui/themed";
 import { StyleSheet } from "react-native";
 import { Button } from "@rneui/base";
 import { PROJECT_ID } from '@env';
+import { useCreateUserMutation } from "../../store/api/usersApi";
 
-const UserForm = () => {
+const UserForm = () => {  
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [feedback, setFeedback] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+
+  const [createUser,] = useCreateUserMutation();
+
+  const submitHandler = () => {
+    if (firstName !== "" && lastName !== "") {
+      setFeedback(`Hej, ${firstName} ${lastName}, välkommen!`);
+      setSubmitted(true);
+      setFirstName("");
+      setLastName("");
+      setTimeout(() => {
+        setFeedback("");
+      }, 5000);
+
+      createUser({
+        user: {
+          firstName,
+          lastName,
+        },
+      });
+    } else {
+      setSubmitted(false);
+      setFeedback("Du måste fylla i alla fält.");
+    }
+  };
+
   return (
     <View>
       <Card>
@@ -15,16 +46,27 @@ const UserForm = () => {
           <Input
             placeholder="Firstname"
             errorStyle={{ color: "red" }}
-            errorMessage="ENTER A VALID ERROR HERE"
+            // errorMessage="ENTER A VALID ERROR HERE"
+            onChangeText={(newFirstName) => setFirstName(newFirstName)}
           />
           <Text style={styles.labelName}>Lastname: </Text>
           <Input
             placeholder="Lastname"
             errorStyle={{ color: "red" }}
-            errorMessage="ENTER A VALID ERROR HERE"
+            // errorMessage="ENTER A VALID ERROR HERE"
+            onChangeText={(newLastName) => setLastName(newLastName)}
           />
         </View>
-        <Button title="Create User" buttonStyle={styles.btnCreate}/>
+        <Button title="Create User"
+          buttonStyle={styles.btnCreate}
+          onPress={submitHandler}
+        />
+        <Text
+          // className={styles.feedbackText}
+          style={{ color: submitted ? "#3c425c" : "#ed4e59" }}
+        >
+          {feedback}
+        </Text>
       </Card>
 
       <Text style={styles.footer}>Project ID: {PROJECT_ID}</Text>
@@ -44,6 +86,11 @@ const styles = StyleSheet.create({
   },
   btnCreate: {
     backgroundColor: 'green',
+  },
+  feedbackText: {
+    // FontFace: 'verdana',
+    // fontWeight: '600',
+    // fontSize: 0.875,
   },
   footer: {
     marginTop: 24,
