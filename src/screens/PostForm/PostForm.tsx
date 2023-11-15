80d76d85d868c57cbc0ec7a5205972a1541f373e
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-11-15 15:10:01 +0100

    add post form screen

---
+++ src/screens/PostForm/PostForm.tsx
@@ -0,0 +1,114 @@
+import React, { useRef, useState } from "react";
+import { View, Text, TouchableWithoutFeedback, Keyboard } from "react-native";
+import { Card, Input } from "@rneui/themed";
+import { StyleSheet } from "react-native";
+import { Button } from "@rneui/base";
+import { useSelector } from "react-redux";
+import { useCreatePostMutation } from "../../store/api/postsApi";
+import { useToast } from "react-native-toast-notifications";
+
+const UserForm = () => {  
+  const [text, setText] = useState("");
+  const [feedback, setFeedback] = useState("");
+  const [submitted, setSubmitted] = useState(false);
+  // const toast = useToast();
+
+  const [createPost,] = useCreatePostMutation();
+
+  const loggedInAs = useSelector((state: any) => state.auth.loggedInAs);
+
+  const submitHandler = () => {
+    if (text !== "") {
+      setFeedback(`Tweet, ${text} `);
+      setSubmitted(true);
+      setText("");
+      setTimeout(() => {
+        setFeedback("");
+      }, 5000);
+
+      createPost({
+        post: {
+          text,
+          createdAt: new Date().toLocaleDateString(),
+          createdBy: loggedInAs.id,
+        }
+      }).then((res) => {
+        if (res) {
+          console.log("success", res)
+          // toast.show("Post created successfully!");
+        } else {
+          console.log("else", res)
+          // toast.show("Post creation failed!");
+        }
+      }).catch((err) => {
+        console.log("errror", err)
+        // toast.show("Post creation failed!");
+      });
+    } else {
+      setSubmitted(false);
+      setFeedback("Du måste fylla i alla fält.");
+    }
+  };
+
+  return (
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+    <View>
+      <Card>
+        <Card.Title>Post Tweet</Card.Title>
+        <Card.Divider />
+        {loggedInAs ? (
+          <>
+          <View style={styles.labels}>
+            <Text style={styles.labelName}>Tweet... </Text>
+            <Input
+              placeholder="Tweet..."
+              returnKeyType="send"
+              errorStyle={{ color: "red" }}
+              // errorMessage="ENTER A VALID ERROR HERE"
+              onChangeText={(newText) => setText(newText)}
+            />
+          </View>
+          <Button title="Publish Tweet"
+            buttonStyle={styles.btnCreate}
+            onPress={submitHandler}
+          />
+          <Text
+            // className={styles.feedbackText}
+            style={{ color: submitted ? "#3c425c" : "#ed4e59" }}
+          >
+            {feedback}
+          </Text>
+          </>
+
+        ) : <Text>Logga in för att börja twittra! </Text> }
+      </Card>
+    </View>
+    </TouchableWithoutFeedback>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  labels: {
+    marginBottom: 24,
+  },
+  labelName: {
+    fontSize: 16,
+  },
+  btnCreate: {
+    backgroundColor: 'green',
+  },
+  feedbackText: {
+    // FontFace: 'verdana',
+    // fontWeight: '600',
+    // fontSize: 0.875,
+  },
+  footer: {
+    marginTop: 24,
+    textAlign: 'center',
+  }
+});
+
+export default UserForm;
