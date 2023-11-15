5b83bf02e84739a6cd73315f64e4407080ec5229
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-11-15 15:12:55 +0100

    update store with auth and posts api

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,13 +1,34 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { usersApi } from './api/usersApi';
+import { postsApi } from './api/postsApi';
+import authSlice from './slices/authSlice';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import storage from 'redux-persist/lib/storage'
+import { persistReducer, persistStore } from "redux-persist";
+
+const persistConfig = {
+  key: "crossplatform-mobile-v1.0.0",
+  storage: storage,
+  whitelist: ["auth"], // Lägg till fler delar av store som du vill spara här.
+};
+
+const persistedReducer = persistReducer(
+  persistConfig,
+  combineReducers({
+    [usersApi.reducerPath]: usersApi.reducer,
+    [postsApi.reducerPath]: postsApi.reducer,
+    auth: authSlice,
+  })
+)
 
 export const store = configureStore({
-  reducer: {
-    [usersApi.reducerPath]: usersApi.reducer,
-  },
+  reducer: persistedReducer,
   middleware: (getDefaultMiddleware) =>
-  getDefaultMiddleware().concat(usersApi.middleware),
+  getDefaultMiddleware().concat(
+    usersApi.middleware,
+    postsApi.middleware,
+    ),
 });
 
 setupListeners(store.dispatch);
