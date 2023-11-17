36d489844aed72b058796cfec5a54ea3c4088291
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-11-17 11:52:27 +0100

    add post form components for create n read posts

--- App.tsx
+++ App.tsx
@@ -1,6 +1,8 @@
 import { StyleSheet } from 'react-native';
 import UserList from './src/screens/UserList/UserList';
 import UserForm from './src/screens/UserForm/UserForm';
+import PostForm from './src/screens/PostForm/PostForm';
+import PostList from './src/screens/PostList/PostList';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
@@ -9,14 +11,18 @@ import { store } from './src/store/store';
 const Tab = createBottomTabNavigator();
 
 export default function App() {
+
+
   return (
     <Provider store={store}>
-    <NavigationContainer>
-      <Tab.Navigator initialRouteName='UserForm'>
-        <Tab.Screen name="UserForm" component={UserForm} options={{title: 'Create User'}}/>
-        <Tab.Screen name="UserList" component={UserList}/>
-      </Tab.Navigator>
-    </NavigationContainer>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName='UserForm'>
+          <Tab.Screen name="UserForm" component={UserForm} options={{title: 'Create User'}}/>
+          <Tab.Screen name="UserList" component={UserList}/>
+          <Tab.Screen name="PostForm" component={PostForm}/>
+          <Tab.Screen name="PostList" component={PostList}/>
+        </Tab.Navigator>
+      </NavigationContainer>
     </Provider>
   );
 }
