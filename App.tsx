d5999b605b215028cb64b10e9ae156f97d4bab85
JohanCodeForFun <hellberg.johan@hotmail.com>
2023-11-23 13:57:57 +0100

    remove unused code and change import arrangement

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-import { StyleSheet } from 'react-native';
-import UserList from './src/screens/UserList/UserList';
-import UserForm from './src/screens/UserForm/UserForm';
-import PostForm from './src/screens/PostForm/PostForm';
-import PostList from './src/screens/PostList/PostList';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { Provider } from 'react-redux';
 import { store } from './src/store/store';
+import UserList from './src/screens/UserList/UserList';
+import UserForm from './src/screens/UserForm/UserForm';
+import PostForm from './src/screens/PostForm/PostForm';
+import PostList from './src/screens/PostList/PostList';
 
 const Tab = createBottomTabNavigator();
 
@@ -26,12 +25,3 @@ export default function App() {
     </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
